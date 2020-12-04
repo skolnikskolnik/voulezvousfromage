04e1483e526b75f92ce240431318287adfdbb294
Julie Altman <jtskolnik@gmail.com>
2020-12-04 17:36:39 -0500

    buttons appear next to the burgers

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -8,17 +8,18 @@ const deleteList = $("#deleteList");
     $.ajax("/burgers", {
         type: "GET"
     }).then(function (data) {
-
+        console.log(data);
         for (let i = 0; i < data.burgers.length; i++) {
             let isDevoured = data.burgers[i].devoured;
             let burgerName = data.burgers[i].burger_name;
+            let burgerId = data.burgers[i].id;
 
             if(!isDevoured){
                 
-                devourList.append(`<li>${burgerName}</li>`);
+                devourList.append(`<li>${burgerName}<button type=button class="devourButton" data-burgerId="${burgerId}">Devour</button></li>`);
             }
             else{
-                deleteList.append(`<li>${burgerName}</li>`);
+                deleteList.append(`<li>${burgerName}<button type=button class="deleteButton" data-burgerId="${burgerId}">Delete</button></li>`);
             }
 
         }
