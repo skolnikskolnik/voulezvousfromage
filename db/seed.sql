14cc38aad2390e6f9d9a7c3dbb55ba2b91b8c8fe
Julie Altman <jtskolnik@gmail.com>
2020-12-04 16:29:39 -0500

    set up directory structure

---
+++ db/seed.sql
@@ -0,0 +1,12 @@
+USE burgers_db;
+
+SELECT*FROM burgers;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("veggie burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("turkey burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("bison burger", false);
