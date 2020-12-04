5d4c25a7ad4d32c7010c4aef3515cc745babd715
Julie Altman <jtskolnik@gmail.com>
2020-12-04 17:10:24 -0500

    html skeleton in place

---
+++ model/burger.js
@@ -0,0 +1,28 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+//Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
+const burger = {
+    selectAll: function(cb){
+        orm.selectAll("burgers", function(res){
+            cb(res);
+        })
+    },
+    insertOne: function(cols,vals,cb){
+        orm.insertOne("burgers", cols, vals, function(res){
+            cb(res);
+        })
+    },
+    updateOne: function(objColVals, condition, cb){
+        orm.updateOne("burgers", objColVals, condition, function(res){
+            cb(res);
+        })
+    },
+    deleteOne: function(condition,cb){
+        orm.deleteOne("burgers", condition, function(res){
+            cb(res);
+        })
+    }
+}
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
