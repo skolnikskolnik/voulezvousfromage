8c6893d00c3ad0c1b2d89188ffcd74395d0f65bd
Julie Altman <jtskolnik@gmail.com>
2020-12-06 12:10:21 -0500

    updated heroku

--- config/connection.js
+++ config/connection.js
@@ -17,15 +17,6 @@ if (process.env.JAWSDB_URL) {
     });
 }
 
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
-
-app.use(express.static("public"));
-
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 
 connection.connect(function (err) {
@@ -37,11 +28,6 @@ connection.connect(function (err) {
     console.log("connected as id " + connection.threadId);
 });
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function () {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-});
 
 // Export connection for our ORM to use.
 module.exports = connection;
